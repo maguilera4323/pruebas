c6d69870f74d523a67d207db1c1cc5bf3072b6fc
Maynor Aguilera <maynoraguileraosorto@gmail.com>
2024-10-26 20:35:34 -0600

    Prueba de commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/' && req.method === 'GET') {
+        // Lee el archivo index.html
+        const filePath = path.join(__dirname, 'index.html');
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                res.statusCode = 500;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end('Error en el servidor');
+            } else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                res.end(data);
+            }
+        });
+    } else {
+        // Respuesta para otras rutas
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<h1>404 - Página no encontrada</h1>');
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Servidor corriendo en http://${hostname}:${port}/`);
+});
